f175cb53a4af83667d89fd28bb021dffbfc8bb4a
Developer <dev@example.com>
2026-02-06 14:06:23 +0530

    database url fix

--- taskmanager/src/index.ts
+++ taskmanager/src/index.ts
@@ -20,7 +20,8 @@ app.use(cors({
       'http://localhost:3000',
       'http://172.20.10.7:3000',
       process.env.FRONTEND_URL, // Vercel Env Variable
-      'https://task-management-system-7mtg.vercel.app' // Aapka Frontend Domain
+      'https://task-management-system-7mtg.vercel.app', // Aapka Frontend Domain
+      'https://task-management-system-7mtg-qnhfk8d3s.vercel.app' // Aapka specific deployment URL
     ];
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
